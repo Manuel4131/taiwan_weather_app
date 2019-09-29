fdec40ba914db49500be0ef8b2b40afb245e7273
Manuel4131 <Manual131419@gmail.com>
2019-09-29 15:21:21 +0800

    Can't track these

--- app/src/main/java/com/alston/cuteweatherapp/config/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.alston.cuteweatherapp.config;
-
-public class Constant {
-    public static final int connectionTimeout = 15;
-    public static final String cwbOpenApiToken =
-            "CWB_API_TOKEN=CWB-9EDAA29C-A9C4-4B6F-A05B-F2AA78FB03B6";
-}
